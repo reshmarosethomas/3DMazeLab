91c51a0f1f4998dcc518decef438a7362731459d
reshmarosethomas <reshmarosethomas@gmail.com>
2023-04-24 23:38:02 -0400

    FinalCuteBuild

--- Assets/Tinylytics_AnalyticsTool/Scripts/BuildInfo.cs
+++ Assets/Tinylytics_AnalyticsTool/Scripts/BuildInfo.cs
@@ -1,1 +1,1 @@
-namespace Tinylytics { public static class BuildInfo{public static string BUILD_TIME = "4/24/2023 9:35:20 PM";}}
+namespace Tinylytics { public static class BuildInfo{public static string BUILD_TIME = "4/24/2023 11:31:40 PM";}}
